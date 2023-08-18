72ddab14cf3cf9221ec1e5919384c14555cbf23f
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-18 14:55:17 -0300

    att

---
+++ Curso-Javascript/Aula 2/script.js
@@ -0,0 +1,13 @@
+// tipos primitivos
+
+// boolean
+var v0uf = false;
+console.log(typeof(v0uf))
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer))
+
+//string 
+var nome = "Gabriel"
+console.log(typeof(nome))
