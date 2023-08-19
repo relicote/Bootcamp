eeb87de05495816e6251e67ac44c2719fad30f77
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-19 10:19:05 -0300

    att

---
+++ Curso-Javascript/Aula 5/script.js
@@ -0,0 +1,11 @@
+// function funcao(){
+//     console.log('Olá, Mundo! Na função?')
+// }
+
+// funcao();
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem('Tudo certo,', 'jovem?');
