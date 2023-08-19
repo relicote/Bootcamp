127d5165779adbc43fec9519d9d40c32260d1d42
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-19 00:07:50 -0300

    Update script.js

--- Curso-Javascript/Aula 4/script.js
+++ Curso-Javascript/Aula 4/script.js
@@ -39,8 +39,40 @@
 let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5',];
 let object = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3',}
 
-//for executa uma instrução até que ela seja falsa
+//for - executa uma instrução até que ela seja falsa
 
-for (let indice = 0; indice < array.length; indice++){
-    console.log(indice)
-}
+// for (let indice = 0; indice < array.length; indice++){
+//     console.log(indice)
+// }
+
+// for/in
+// for (let i in array){
+//     console.log(i)
+// }
+
+// com object
+// for (i in object){
+//     console.log(i)
+// }
+
+// for/of - executa repetição a partir de valor com array
+// for (i of array){
+//     console.log(i)
+// }
+
+// //com object
+// for (i of object.propriedade1){
+//     console.log(i)
+// }
+
+var a = 0;
+
+// while (a < 10 ){
+//     a++;
+//     console.log(a);
+// }
+
+do {
+    a++;
+    console.log(a);
+} while (a < 10);
