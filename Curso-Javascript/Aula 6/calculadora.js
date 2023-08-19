4f64c8b80f1782efc3063c69a12876b9e4626664
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-19 10:42:30 -0300

    Update calculadora.js

--- Curso-Javascript/Aula 6/calculadora.js
+++ Curso-Javascript/Aula 6/calculadora.js
@@ -2,6 +2,38 @@
 // Criando a função da calculadora
 function calculadora(){
     const operacao = prompt('Escolha uma operação:\n 1- Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n  4 - Divisão real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)');
+
+
+let n1 = Number(prompt('Insira o primeiro valor:'));
+let n2 = Number(prompt('Insira o segundo valor:'));
+let resultado;
+
+// criando funções para cada operador
+
+function soma(){
+    resultado = n1 + n2;
+    alert(`${n1} + ${n2} = ${resultado}`);
+}
+
+function subtracao(){
+    resultado = n1 - n2;
+    alert(`${n1} - ${n2} = ${resultado}`);
+}
+function multiplicacao(){
+    resultado = n1 * n2;
+    alert(`${n1} * ${n2} = ${resultado}`);
+}
+function divisaoReal(){
+    resultado = n1 / n2;
+    alert(`${n1} / ${n2} = ${resultado}`);
+}
+function divisaoInteira(){
+    resultado = n1 % n2;
+    alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
+}
+function potenciacao(){
+    resultado = n1 ** n2;
+    alert(`${n1} elevado a ${n2} é igual a ${resultado}`);
 }
 
 // Selecionando o operador
@@ -19,4 +51,5 @@ if (operacao == 1){
     potenciacao();
 }
 
+}
 calculadora();
