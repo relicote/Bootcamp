936e9bc8f7f655013eafaef3748ab578bb687d86
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-18 22:59:45 -0300

    Update script.js

--- Curso-Javascript/Aula 2/script.js
+++ Curso-Javascript/Aula 2/script.js
@@ -44,5 +44,39 @@
 
 //Comparação identica
 
-var comparacaoIdentica = '0' === '0';
-console.log(comparacaoIdentica)
+// var comparacaoIdentica = '0' === '0';
+// console.log(comparacaoIdentica)
+
+// Adição
+// var adicao = 1 + 1;
+// console.log(adicao)
+
+// Subtração
+// var sub = 2 - 1;
+// console.log(sub)
+
+
+// Multiplicação
+// var mult = 2 * 3;
+// console.log(mult)
+
+// Divisão Real
+// var divReal = 6 / 2;
+// console.log(divReal)
+
+//Divisão inteira
+// var divisaoInteira = 5 % 2;
+// console.log(divisaoInteira)
+
+//Potenciação
+// var poten = 2 ** 10;
+// console.log(poten)
+
+// Maior que 
+var maiorQue = 5 >= 5 ;
+console.log(maiorQue)
+
+// OP Lógicos
+// &&  "e" - Considera que todos os valores sejam true 
+// || "ou" - Considera que qualquer valor seja true
+// ! - "não" - Inverte o valor de true para false ou vice-versa
