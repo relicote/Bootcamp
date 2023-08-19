8deaebc4692b932ebda62d8eff2747e78d6cc1fe
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-19 10:33:09 -0300

    att

---
+++ Curso-Javascript/Aula 6/script.js
@@ -0,0 +1,11 @@
+// var funcao = function(){
+//     console.log('Sou uma mensagem de expressão');
+// }
+
+// funcao();
+
+// var funcao = () => {
+//     console.log('Sou uma arrow function')
+// }
+
+// funcao()
