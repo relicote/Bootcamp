556c998926241b388bf6e63cfb6efebdc7bec660
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-19 11:01:53 -0300

    Update calculadora.js

--- Curso-Javascript/Aula 6/calculadora.js
+++ Curso-Javascript/Aula 6/calculadora.js
@@ -1,39 +1,66 @@
 
 // Criando a função da calculadora
 function calculadora(){
-    const operacao = prompt('Escolha uma operação:\n 1- Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n  4 - Divisão real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)');
+    const operacao = Number(prompt('Escolha uma operação:\n 1- Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n  4 - Divisão real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)'));
 
+    if (!operacao || operacao >= 7){
+        alert('Erro - Operação inválida!');
+        calculadora();
+    } else {
 
-let n1 = Number(prompt('Insira o primeiro valor:'));
-let n2 = Number(prompt('Insira o segundo valor:'));
-let resultado;
+    let n1 = Number(prompt('Insira o primeiro valor:'));
+    let n2 = Number(prompt('Insira o segundo valor:'));
+    let resultado;
 
-// criando funções para cada operador
+    if (!n1 || !n2){
+        alert('Erro - parâmetros inválidos!');
+        calculadora();
+    } else {
+        // criando funções para cada operador
 
-function soma(){
-    resultado = n1 + n2;
-    alert(`${n1} + ${n2} = ${resultado}`);
-}
+    function soma(){
+        resultado = n1 + n2;
+        alert(`${n1} + ${n2} = ${resultado}`);
+        novaop();
+    }
 
-function subtracao(){
-    resultado = n1 - n2;
-    alert(`${n1} - ${n2} = ${resultado}`);
-}
-function multiplicacao(){
-    resultado = n1 * n2;
-    alert(`${n1} * ${n2} = ${resultado}`);
-}
-function divisaoReal(){
-    resultado = n1 / n2;
-    alert(`${n1} / ${n2} = ${resultado}`);
-}
-function divisaoInteira(){
-    resultado = n1 % n2;
-    alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
-}
-function potenciacao(){
-    resultado = n1 ** n2;
-    alert(`${n1} elevado a ${n2} é igual a ${resultado}`);
+    function subtracao(){
+        resultado = n1 - n2;
+        alert(`${n1} - ${n2} = ${resultado}`);
+        novaop();
+    }
+    function multiplicacao(){
+        resultado = n1 * n2;
+        alert(`${n1} * ${n2} = ${resultado}`);
+        novaop();
+    }
+    function divisaoReal(){
+        resultado = n1 / n2;
+        alert(`${n1} / ${n2} = ${resultado}`);
+        novaop();
+    }
+    function divisaoInteira(){
+        resultado = n1 % n2;
+        alert(`O resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`);
+        novaop();
+    }
+    function potenciacao(){
+        resultado = n1 ** n2;
+        alert(`${n1} elevado a ${n2} é igual a ${resultado}`);
+        novaop();
+    }
+
+    function novaop(){
+        let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+        if (opcao == 1){
+            calculadora();
+        } else if (opcao == 2) {
+            alert('Até mais!');
+        } else {
+            alert('Digite uma opção válida!');
+            novaop();
+        }
+    }
 }
 
 // Selecionando o operador
@@ -51,5 +78,6 @@ if (operacao == 1){
     potenciacao();
 }
 
+    }
 }
 calculadora();
