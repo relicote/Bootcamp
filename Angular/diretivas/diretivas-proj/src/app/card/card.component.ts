b08315bae287722b648980a39cd95bc92451aea2
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-08-31 13:41:22 -0300

    att

--- Angular/diretivas/diretivas-proj/src/app/card/card.component.ts
+++ Angular/diretivas/diretivas-proj/src/app/card/card.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  produtos:string[] = []
+
+  constructor() {
+    this.produtos = [
+      "mouse",
+      'teclado',
+      'cabo',
+      'fonte'
+    ]
+  }
 
   ngOnInit(): void {
+  }
+
+  adicionar(){
+    this.produtos.push('Gabriel')
   }
 
 }
